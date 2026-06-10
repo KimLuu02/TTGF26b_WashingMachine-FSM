a043436f2d2e615cecfb4025ffdf7ee485a17d04
KimLuu02 <kimlinhluu02@gmail.com>
2026-06-10 18:40:10 +0200

    fixing problem with internal signals
    Internal signals can only be accessed if there is no gate level testing

--- test/testbench.py
+++ test/testbench.py
@@ -2,10 +2,14 @@ import cocotb
 from cocotb.triggers import Timer, RisingEdge, ReadOnly, ClockCycles
 from cocotb.clock import Clock
 
+import os
+
+GATE_LEVEL = os.getenv("GATES") == "yes"
+
 @cocotb.test()
 async def req_1_mode_select(dut):
     """ REQ-1: the selected washing mode shall be forwarded by the UI module """
-    cocotb.start_soon(Clock(dut.clk, 10, units="ns").start())
+    cocotb.start_soon(Clock(dut.clk, 20, units="ns").start())
 
     #Set initial inputs
     dut.start.value = 0
@@ -27,10 +31,10 @@ async def req_1_mode_select(dut):
 
     dut._log.info("Req-1 Passed")
 
-@cocotb.test()
+@cocotb.test(skip=GATE_LEVEL)
 async def req_2_door_open_warning(dut):
     """ REQ-2: If the door is open, the washing cycle should not start and a warning signal should be given """
-    cocotb.start_soon(Clock(dut.clk, 10, units="ns").start())
+    cocotb.start_soon(Clock(dut.clk, 20, units="ns").start())
 
     #Set initial inputs
     dut.door_closed.value = 0
@@ -40,16 +44,16 @@ async def req_2_door_open_warning(dut):
     # Testing
     dut.start.value = 1
     await RisingEdge(dut.clk)
-
-    assert dut.warning.value == 1, "REQ-2 failed: warning was not active"
-    assert dut.start_cycle.value == 0, "REQ-2 failed: start_cycle started"
+    
+    assert dut.top_system.warning.value == 1, "REQ-2 failed: warning was not active"
+    assert dut.top_system.start_cycle.value == 0, "REQ-2 failed: start_cycle started"
 
     dut._log.info("Req-2 Passed")
 
-@cocotb.test()
+@cocotb.test(skip=GATE_LEVEL)
 async def req_3_reset(dut):
     """ REQ-3: If the user presses the reset button, all modules should go to their reset state """
-    cocotb.start_soon(Clock(dut.clk, 10, units="ns").start())
+    cocotb.start_soon(Clock(dut.clk, 20, units="ns").start())
 
     #Set initial inputs
     dut.reset_in.value = 1
@@ -61,15 +65,15 @@ async def req_3_reset(dut):
     dut.reset_in.value = 0
     await RisingEdge(dut.clk)
 
-    assert dut.reset_out.value == 0, "REQ-3 failed: reset_out was not 0"
-    assert dut.timer_en.value == 0, "REQ-3 failed: timer was enabled"
+    assert dut.top_system.reset_out.value == 0, "REQ-3 failed: reset_out was not 0"
+    assert dut.top_system.timer_en.value == 0, "REQ-3 failed: timer was enabled"
 
     dut._log.info("Req-3 Passed")
 
-@cocotb.test()
+@cocotb.test(skip=GATE_LEVEL)
 async def req_4_start_cycle(dut):
     """ REQ-4: The washing cycle or warning should only start if the user pressed the start button """
-    cocotb.start_soon(Clock(dut.clk, 10, units="ns").start())
+    cocotb.start_soon(Clock(dut.clk, 20, units="ns").start())
 
     #Set initial inputs
     dut.reset_in.value = 1
@@ -81,16 +85,16 @@ async def req_4_start_cycle(dut):
     dut.start.value = 0
     await RisingEdge(dut.clk)
 
-    assert dut.start_cycle.value == 0, "REQ-4 failed: Cycle started"
-    assert dut.timer_en.value == 0, "REQ-4 failed: timer was enabled"
-    assert dut.warning.value == 0, "REQ-4 failed: there was a warning"
+    assert dut.top_system.start_cycle.value == 0, "REQ-4 failed: Cycle started"
+    assert dut.top_system.timer_en.value == 0, "REQ-4 failed: timer was enabled"
+    assert dut.top_system.warning.value == 0, "REQ-4 failed: there was a warning"
 
     dut._log.info("Req-4 Passed")
 
 @cocotb.test()
 async def req_5_warning_LED(dut):
     """ REQ-5: If the door is not closed, the washing machine should warn the user """
-    cocotb.start_soon(Clock(dut.clk, 10, units="ns").start())
+    cocotb.start_soon(Clock(dut.clk, 20, units="ns").start())
 
     #Set initial inputs
     dut.reset_in.value = 1
@@ -103,7 +107,8 @@ async def req_5_warning_LED(dut):
 
     await ReadOnly()
 
-    assert dut.warning.value == 1, "REQ-5 failed: Warning is not on"
+    if GATE_LEVEL:
+        assert dut.top_system.warning.value == 1, "REQ-5 failed: Warning is not on"
     assert dut.done_led.value == 1, "REQ-5 failed: LED not on"
 
     dut._log.info("Req-5 Passed")
@@ -111,7 +116,7 @@ async def req_5_warning_LED(dut):
 @cocotb.test()
 async def req_6_warning_LED(dut):
     """ REQ-6:  After succefully starting the washing machine, the washing cycle begins """
-    cocotb.start_soon(Clock(dut.clk, 11, units="ns").start())
+    cocotb.start_soon(Clock(dut.clk, 20, units="ns").start())
 
     #Set initial inputs
     dut.reset_in.value = 0
@@ -131,8 +136,9 @@ async def req_6_warning_LED(dut):
 
     await ReadOnly()
 
-    assert dut.timer_en.value == 1, "REQ-6 failed: timer not enabled"
-    assert dut.warning.value == 0, "REQ-6 failed: Warning active"
+    if GATE_LEVEL:
+        assert dut.top_system.timer_en.value == 1, "REQ-6 failed: timer not enabled"
+        assert dut.top_system.warning.value == 0, "REQ-6 failed: Warning active"
     assert dut.water_valve.value == 1, "REQ-6 failed: water valve was not turned on"
 
     dut._log.info("Req-6 Passed")
@@ -208,7 +214,7 @@ async def req_7_wash_cycles(dut):
   
     dut._log.info("Req-7 Passed")
 
-@cocotb.test()
+@cocotb.test(skip=GATE_LEVEL)
 async def req_8_9_timer(dut):
     """ REQ-8 and 9:  After receiving the signal to start, the timer should start counting the cycles and output a signal, after the timer is done"""
     cocotb.start_soon(Clock(dut.clk, 20, units="ns").start())
@@ -233,17 +239,17 @@ async def req_8_9_timer(dut):
     await ReadOnly()
 
     # FILL
-    assert dut.timer_en.value == 1, "REQ-8 failed: Timer did not start"
+    assert dut.top_system.timer_en.value == 1, "REQ-8 failed: Timer did not start"
 
     await ClockCycles(dut.clk, 4) # 3 cycles
     await ReadOnly()
 
     # Timer check
-    assert dut.timer_done.value == 1, "REQ-9 failed: Timer did not stop"
+    assert dut.top_system.timer_done.value == 1, "REQ-9 failed: Timer did not stop"
 
     dut._log.info("Req-8 and 9 Passed")
 
-@cocotb.test()
+@cocotb.test(skip=GATE_LEVEL)
 async def req_10_duration(dut):
     """ REQ-10:  The user can select a mode which changes the duration of each washing cycle"""
     cocotb.start_soon(Clock(dut.clk, 20, units="ns").start())
@@ -272,6 +278,6 @@ async def req_10_duration(dut):
     await ReadOnly()
 
     # Timer check
-    assert dut.timer_done.value == 1, "REQ-10 failed: Timer did not stop after 5 cycles for normal mode for FILL"
+    assert dut.top_system.timer_done.value == 1, "REQ-10 failed: Timer did not stop after 5 cycles for normal mode for FILL"
 
     dut._log.info("Req-10 Passed")
