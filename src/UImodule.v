8fa6c9b8404a0101850ca44699c2ba24396a5f0c
KimLuu02 <kimlinhluu02@gmail.com>
2026-06-15 22:33:15 +0200

    hopefully fixing syntax of UImodule

--- src/UImodule.v
+++ src/UImodule.v
@@ -6,10 +6,10 @@ module ui_module(
     input wire mode_select,
     input wire reset_in,
 
-    output wire start_cycle,
-    output wire warning,
-    output wire mode,
-    output wire reset_out
+    output reg start_cycle,
+    output reg warning,
+    output reg mode,
+    output reg reset_out
 
 );
 
@@ -28,23 +28,17 @@ always @(*) begin
 end
 
 always @(*) begin
-    if ((start & ~door_closed) == 1'b1) begin
-        warning = 1'b1;
-    end else begin
-        warning = 1'b0;
-    end
-end
-
-always @(*) begin
     if ((start & door_closed) == 1'b1) begin
+        warning = 1'b0;
         start_cycle = 1'b1;
     end else begin
+        warning = 1'b1;
         start_cycle = 1'b0;
     end
 end
 
 always @(*) begin
-    if ((reset_in) == 1'b1) begin
+    if (reset_in == 1'b1) begin
         reset_out = 1'b1;
     end else begin
         reset_out = 1'b0;
